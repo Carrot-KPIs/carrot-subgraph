cd33506893324e8731537e60bd253781463359a4
Federico Luzzi <fedeluzzi00@gmail.com>
2021-10-04 14:33:51 +0200

    chore: new factory

--- src/commons/addresses.ts
+++ src/commons/addresses.ts
@@ -4,7 +4,7 @@ import { ADDRESS_ZERO } from './constants'
 export function getFactoryAddress(): string {
   let network = dataSource.network() as string
   // not using a switch-case because using strings is not yet supported (only u32)
-  if (network == 'rinkeby') return '0x5e02b60e676cf965deb29df522fe26f0c1d5d771'
+  if (network == 'rinkeby') return '0x6752241ee8420cb61a6ae6b666bd5759bfac6eb0'
   log.warning('no factory address for unsupported network {}', [network])
   return ADDRESS_ZERO
 }
