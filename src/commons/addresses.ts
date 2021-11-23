301c078207decd73fb4d71742946a538bcc3dc2f
Federico Luzzi <fedeluzzi00@gmail.com>
2021-11-23 10:23:47 +0100

    chore: new factory address on xdai

--- src/commons/addresses.ts
+++ src/commons/addresses.ts
@@ -7,7 +7,7 @@ export function getFactoryAddress(): string {
   let network = dataSource.network() as string
   // not using a switch-case because using strings is not yet supported (only u32)
   if (network == 'rinkeby') return '0x6752241ee8420cb61a6ae6b666bd5759bfac6eb0'
-  if (network == 'xdai') return '0xe9c1c9722bbe9e36489e16c095641b9c803ceacb'
+  if (network == 'xdai') return '0xe82c4d8b993d613a28600b953e91a3a93ae69fd6'
   log.warning('no factory address for unsupported network {}', [network])
   return ADDRESS_ZERO
 }
